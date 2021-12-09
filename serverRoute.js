85474b6f420685e1d176032410ea93f8beb7037a
Manjil Ghale <rilleymanjil@hotmail.com>
2021-12-09 04:44:10 +0000

    Route input + add Data to MongoDB (Incomplete)

--- serverRoute.js
+++ serverRoute.js
@@ -1,42 +1,16 @@
 const express = require('express')
-var cors = require('cors')
-const covid_Doc = require('./index')
+const covid_doc = require('./index')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express();
-const PORT = 8080;
 const routes = express.Router();
-
-// Data Parsing
-// Enabling Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended:false }));
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/all-covid-Data', (req, res) => {
-    covid_Doc.find({})
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
+app.use(cors());
 
-app.post('/add-data', (req, res) => {
-    console.log(req.body);
-    const newData = new covid_Doc(req.body);
-    console.log('New Data = ' + newData);
-    newData.save()
-    .then(function(value) {
-        console.log(value);
-    })
-    .catch(err => {
-        res.status(400).send('Adding new Data failed');
-    })
-})
+const PORT = 8080;
 
 routes.route('/').get(function(req, res) {
     covid_doc.find(function(err, docs) {
@@ -63,6 +37,6 @@ routes.route('/add-data').post(function(req, res) {
 
 app.use('/docs', routes);
 
-app.listen(PORT, function() {
-    console.log(`Server started ${PORT}`);
-});
+app.listen(8080, function() {
+    console.log(`Server has begun at port ${PORT}`);
+})
